7f43a49e5f7a759b4a5559cdc5634f9d986d1ed1
Miguel D <migdusa@gmail.com>
2012-10-27 11:03:30 -0500

    definición parcial de objetos de tablas en código

---
+++ src/com/diplomadoUNAL/geosalesman/database/ClientTable.java
@@ -0,0 +1,10 @@
+package com.diplomadoUNAL.geosalesman.database;
+
+public class ClientTable {
+	public static final String ID = "_id";
+	public static final String TABLE_NAME = "client";
+	public static final String NAME = "name"; //client name
+	public static final String PHONE_NUMBER = "phone_number";
+	public static final String ADDRESS = "address";
+	public static final String CONTACT_NAME = "contact_name";
+}
--- src/com/diplomadoUNAL/geosalesman/database/QuestionTable.java
+++ src/com/diplomadoUNAL/geosalesman/database/QuestionTable.java
@@ -1,5 +1,10 @@
 package com.diplomadoUNAL.geosalesman.database;
 
 public class QuestionTable {
-
+	public static final String ID = "_id";
+	public static final String TABLE_NAME = "question";
+	public static final String QUESTION_DESCRIPTION = "question_description";
+	public static final String QUESTION = "question";
+	public static final String QUESTION_TYPE ="question_type";
+	public static final String ANSWER_OPTIONS = "answer_options";	
 }
