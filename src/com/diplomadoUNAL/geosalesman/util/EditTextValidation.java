3284079245dce351ef63c4d9edcf737bace793a2
Miguel D <migdusa@gmail.com>
2012-11-03 17:40:53 -0500

    Validations fixed

--- src/com/diplomadoUNAL/geosalesman/util/EditTextValidation.java
+++ src/com/diplomadoUNAL/geosalesman/util/EditTextValidation.java
@@ -1,41 +1,38 @@
 package com.diplomadoUNAL.geosalesman.util;
 
+import java.util.HashMap;
 import java.util.Iterator;
-
-import android.util.SparseArray;
-import android.view.ViewDebug.FlagToString;
-import android.widget.EditText;
 
 public class EditTextValidation {
 	public static final int ALPHABETHIC_VALIDATION = 1; // only alphabethic and
 	// spaces allowed
 	public static final int NUMBER_VALIDATION = 2;
-	public static final int NUMBER_WITH_SPACES_VALIDATION = 6;
-	public static final int NO_PIPE_CHAR_VALIDATION = 3; // this character is
+	public static final int NUMBER_WITH_SPACES_VALIDATION = 3;
+	public static final int NO_PIPE_CHAR_VALIDATION = 4; // this character is
 	// special, so it is
 	// not allowed as input
-	public static final int RANGE_VALIDATION = 4;
-	public static final int CHARACTER_VALIDATION = 5;
+	public static final int CHARACTER_VALIDATION = 8;
 
 	/**
 	 * 
 	 * @param editText
 	 * @param validationTest
 	 *            k: validation test; v: error message to shown into edit test in case of error
-	 * @return True if all tests are Ok. False otherwhise.
+	 * @return String with error, null if no error found
 	 */
-	public static boolean editTextValidation(EditText editText,
-					SparseArray<String> validationTest) {
-		String textToValidate = editText.getText().toString();
+	public static String editTextValidation(String textToValidate,
+					HashMap<Integer, String> validationTest) {
+
 		int size = validationTest.size();
 		StringBuilder stringBuilder = new StringBuilder();
-		int key = 0;
-		for (int i = 0; i < size; i++) {
-			key = validationTest.keyAt(i);
+		Iterator<Integer> keyIter = validationTest.keySet().iterator();
+		while (keyIter.hasNext()) {
+			Integer key = keyIter.next();
+
 			boolean flagMatchCase = false;
-			switch (key) {
+			switch (key.intValue()) {
 			case ALPHABETHIC_VALIDATION:
-				if (!textToValidate.matches("[a-zA-Z\\s']+"))
+				if (!textToValidate.matches("[a-zA-Z ‡Ž’—œAƒêîò„–\\s']+"))
 					flagMatchCase = true;
 				break;
 			case NUMBER_VALIDATION:
@@ -44,31 +41,32 @@ public class EditTextValidation {
 				break;
 
 			case NUMBER_WITH_SPACES_VALIDATION:
-				if (!textToValidate.matches("[0-9\\s']+"))
+				if (!textToValidate.matches("[0-9 ]+"))
 					flagMatchCase = true;
 				break;
 			case NO_PIPE_CHAR_VALIDATION:
-				if (!textToValidate.contains("|"))
+				if (textToValidate.contains("|"))
 					flagMatchCase = true;
 				break;
-			case RANGE_VALIDATION:
-				//TODO Implement this
-				break;
 			case CHARACTER_VALIDATION:
-				//TODO Implement this
+				// TODO Change to a regex
+				if (textToValidate.contains("'")
+								|| textToValidate.contains("\"")
+								|| textToValidate.contains("+")
+								|| textToValidate.contains("*"))
+					flagMatchCase = true;
 				break;
 			}
 			if (flagMatchCase) {
-				stringBuilder.append(validationTest.get(key));
+				stringBuilder.append(validationTest.get(key.intValue()));
 				stringBuilder.append('\n');
 			}
 		}
 		if (stringBuilder.length() > 0) {
-			stringBuilder.delete(stringBuilder.length()-1, stringBuilder.length());
-			editText.setError(stringBuilder.toString());
-			return false;
+			stringBuilder.delete(stringBuilder.length() - 1,
+							stringBuilder.length());
+			return stringBuilder.toString();
 		} else
-			editText.setError(null);
-		return true;
+			return null;
 	}
 }
